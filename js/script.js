45e5093e4066d11dcae4ac774d042b515e76e6c3
DzmitryMiankou <gmiankou@gmail.com>
2024-04-17 20:19:42 +0300

    oi

--- js/script.js
+++ js/script.js
@@ -3,21 +3,37 @@
 window.addEventListener("load", function () {
   start();
 });
+
 const whatDevice = /iPad|iPhone|iPod/.test(navigator.userAgent);
 const whatEventUse = whatDevice ? "touchstart" : "click";
 const buttons = [...document.querySelectorAll(".buttons__button")];
 const cardsAll = [...document.querySelectorAll(".product-card")];
 
 function cards() {
-  function dd(params) {
-    cardsAll.forEach((el) => {
-      if (params.trim() === el.children[0].textContent.trim())
-        return console.log(params.trim());
-    });
+  for (let i = 0; i !== cardsAll.length; i++) {
+    cardsAll[i].classList.add(`product-card--${i + 1}`);
+  }
+
+  function controlStyleCards(params) {
+    const activeCard = cardsAll.find(
+      (el) => el.children[0].textContent.trim() === params.trim()
+    );
+    const oldClass = activeCard.className.split(" ")[1];
+
+    const delActiveCard = cardsAll.find(
+      (el) => el.className.split(" ")[1] === `product-card--1`
+    );
+
+    cardsAll.forEach((el) => el.classList.remove("product-card--1"));
+    delActiveCard.classList.add(oldClass);
+    activeCard.classList.remove(oldClass);
+    activeCard.classList.add(`product-card--1`);
   }
 
   buttons.forEach((el) =>
-    el.addEventListener(whatEventUse, () => dd(el.textContent))
+    el.addEventListener(whatEventUse, (ev) =>
+      controlStyleCards(ev.target.textContent)
+    )
   );
 }
 
